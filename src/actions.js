de5121716053a74855529bd542934c1285ee4273
Richard Ramos <info@richardramos.me>
2019-05-22 18:16:41 -0400

    refactor: added spinner to wait for trxs, and varios transaction related refactors

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,7 @@
 var inquirer = require('inquirer');
 const Web3 = require("web3");
-const Table = require('cli-table');
 const PledgeAdminUtils = require('./pledgeadmin-utils');
-
+const TrxUtils = require('./trx-utils');
 const Contracts = require("./contracts.js");
 
 const web3 = new Web3();
@@ -59,18 +58,18 @@ class Actions {
   addProject(params) {
     let text = `await LiquidPledging.methods.addProject(\"${params.name}\", \"${params.url}\", \"${params.account}\", ${params.parentProject}, ${params.commitTime}, \"${params.plugin}\").send({from: \"${web3.eth.defaultAccount}\", gas: 2000000})`
     doAction(text, async () => {
-      let projectReceipt = await this.contracts.LiquidPledging.methods.addProject(params.name, params.url, params.account, params.parentProject, params.commitTime, params.plugin).send({from: web3.eth.defaultAccount, gas: 2000000});
-      console.dir("txHash: " + projectReceipt.transactionHash)
-      var projectId = projectReceipt.events.ProjectAdded.returnValues.idProject;
-
-      console.log(projectId);
+      const toSend = this.contracts.LiquidPledging.methods.addProject(params.name, params.url, params.account, params.parentProject, params.commitTime, params.plugin);
+      const receipt = await TrxUtils.executeAndWait(toSend, web3.eth.defaultAccount);
+      console.dir("txHash: " + receipt.transactionHash);
+      const projectId = receipt.events.ProjectAdded.returnValues.idProject;
+      console.log("Project ID: " , projectId);
     });
   }
 
   async listProjects() {
     try {
       const pledgeAdmins = await PledgeAdminUtils.getPledgeAdmins(this.contracts.LiquidPledging);
-      PledgeAdminUtils.printTable(pledgeAdmins.filter(x => x !== PledgeAdminUtils.constants.PROJECT));
+      PledgeAdminUtils.printTable(pledgeAdmins.filter(x => x.adminType === PledgeAdminUtils.constants.PROJECT));
     } catch(error){
       console.log(error);
       console.log("Couldn't obtain the list of projects: ", error.message);
@@ -82,7 +81,7 @@ class Actions {
     doAction(text, async () => {
       try {
         const pledgeAdmin = await this.contracts.LiquidPledging.methods.getPledgeAdmin(params.id).call();
-        PledgeAdminUtils.printTable([pledgeAdmin].filter(x => x !== PledgeAdminUtils.constants.PROJECT));
+        PledgeAdminUtils.printTable([pledgeAdmin].filter(x => x.adminType === PledgeAdminUtils.constants.PROJECT));
       } catch(error){
         console.log("Couldn't obtain the project: ", error.message);
       }
@@ -92,7 +91,7 @@ class Actions {
   async listFunders() {
     try {
       const pledgeAdmins = await PledgeAdminUtils.getPledgeAdmins(this.contracts.LiquidPledging);
-      PledgeAdminUtils.printTable(pledgeAdmins.filter(x => x !== PledgeAdminUtils.constants.FUNDER));
+      PledgeAdminUtils.printTable(pledgeAdmins.filter(x => x.adminType === PledgeAdminUtils.constants.FUNDER));
     } catch(error){
       console.log(error);
       console.log("Couldn't obtain the list of funders: ", error.message);
@@ -102,34 +101,38 @@ class Actions {
   addGiver(params) {
     let text = `await LiquidPledging.methods.addGiver(\"${params.name}\", \"${params.url}\", ${params.commitTime}, \"${params.plugin}\").send({from: \"${web3.eth.defaultAccount}\", gas: 2000000})`
     doAction(text, async () => {
-      let funderReceipt = await this.contracts.LiquidPledging.methods.addGiver(params.name, params.url, params.commitTime, params.plugin).send({from: web3.eth.defaultAccount, gas: 2000000})
-      console.dir("txHash: " + funderReceipt.transactionHash)
-      var funderId = funderReceipt.events.GiverAdded.returnValues.idGiver;
-      console.log(funderId);
+      const toSend = this.contracts.LiquidPledging.methods.addGiver(params.name, params.url, params.commitTime, params.plugin);
+      const receipt = await TrxUtils.executeAndWait(toSend, web3.eth.defaultAccount);
+      console.dir("txHash: " + receipt.transactionHash);
+      const funderId = receipt.events.GiverAdded.returnValues.idGiver;
+      console.log("Funder ID: " , funderId);
     });
   }
 
   mintToken(params) {
     let text = `await StandardToken.methods.mint(\"${params.account}\", web3.utils.toWei(\"${params.amount}\", \"ether\")).send({gas: 2000000})`
     doAction(text, async () => {
-      let mintReceipt = await this.contracts.StandardToken.methods.mint(params.account, web3.utils.toWei(params.amount, "ether")).send({gas: 2000000})
-      console.dir("txHash: " + mintReceipt.transactionHash)
+      const toSend = this.contracts.StandardToken.methods.mint(params.account, web3.utils.toWei(params.amount.toString(), "ether"));
+      const receipt = await TrxUtils.executeAndWait(toSend, web3.eth.defaultAccount);
+      console.dir("txHash: " + receipt.transactionHash);
     });
   }
 
   approveToken(params) {
     let text = `await StandardToken.methods.approve(\"${this.contracts.LiquidPledging.options.address}\", web3.utils.toWei(\"${params.amount}\", \"ether\")).send({gas: 2000000})`
     doAction(text, async () => {
-      let mintReceipt = await this.contracts.StandardToken.methods.approve(this.contracts.LiquidPledging.options.address, web3.utils.toWei(params.amount, "ether")).send({gas: 2000000})
-      console.dir("txHash: " + mintReceipt.transactionHash)
+      const toSend = this.contracts.StandardToken.methods.approve(this.contracts.LiquidPledging.options.address, web3.utils.toWei(params.amount.toString(), "ether"));
+      const receipt = await TrxUtils.executeAndWait(toSend, web3.eth.defaultAccount);
+      console.dir("txHash: " + receipt.transactionHash);
     });
   }
 
   donate(params) {
     let text = `await LiquidPledging.methods.donate(${params.funderId}, ${params.projectId}, \"${this.contracts.LiquidPledging.options.address}\", web3.utils.toWei(\"${params.amount}\", \"ether\")).send({gas: 2000000});`
     doAction(text, async () => {
-      let donateReceipt = await this.contracts.LiquidPledging.methods.donate(params.funderId, params.projectId, this.contracts.LiquidPledging.options.address, web3.utils.toWei(params.amount, "ether")).send({gas: 2000000});
-      console.dir("txHash: " + donateReceipt.transactionHash)
+      const toSend = this.contracts.LiquidPledging.methods.donate(params.funderId, params.projectId, this.contracts.LiquidPledging.options.address, web3.utils.toWei(params.amount.toString(), "ether"));
+      const receipt = await TrxUtils.executeAndWait(toSend, web3.eth.defaultAccount);
+      console.dir("txHash: " + receipt.transactionHash);
     });
   }
 
---
+++ src/trx-utils.js
@@ -0,0 +1,22 @@
+const Spinner = require('cli-spinner').Spinner;
+
+const executeAndWait = async (toSend, account) => {
+  const spinner = new Spinner('%s');
+  spinner.setSpinnerString(18);
+  spinner.start();
+
+  try {
+    const estimatedGas = await toSend.estimateGas({from: account});
+    const receipt = await toSend.send({from: account, gas: estimatedGas + 10000});
+
+    spinner.stop(true);
+    return receipt; 
+  } catch(error) {
+    console.log("Error minting tokens: ", error.message);
+    spinner.stop(true);
+  }
+}
+
+module.exports = {
+  executeAndWait
+};
